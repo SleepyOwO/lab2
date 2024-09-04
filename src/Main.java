f318acee8c37cedb7840168cf20398df51f1e83f
SleepyOwO <fostershinigami@gmail.com>
2024-09-04 23:50:45 +0300

    Лабораторная работа 2

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.regex.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Pattern reg = Pattern.compile("^#([a-fA-F0-9]{6})");
+
+        String test = in.next();
+
+        Matcher match = reg.matcher(test);
+        Boolean res = match.matches();
+
+        System.out.println(res);
+    }
+}
